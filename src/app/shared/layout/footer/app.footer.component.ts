839be9f7790fb09117d7d0edd5dc83eb626034a5
DESKTOP-179HKOP\LocalAdmin <z.mayel@hotmail.com>
2020-02-08 19:35:03 +0100

    set the content to full screen when the sidebar ist hiiden

--- src/app/shared/layout/footer/app.footer.component.ts
+++ src/app/shared/layout/footer/app.footer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-footer',
+    selector: 'app-footer, [app-footer]',
     template: `
         <div class="layout-footer clearfix">
             <span class="footer-text-right">
